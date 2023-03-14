6ce8b4330c59f02753c2d4742b3b94a8c1abf693
Cristina Taboada <cris.taboada98@gmail.com>
2023-03-14 13:52:38 +0100

    Reto 4

--- POO/poo.js
+++ POO/poo.js
@@ -1,11 +1,10 @@
 //-- Clases
 class Person{
     //-- Constructor
-    constructor(name,lastName,age,weight,height,nationality,yearOfBirth){
+    constructor(name,lastName,weight,height,nationality,yearOfBirth){
         //-- Atributos
         this.name = name;
         this.lastName = lastName;
-        this.age = age;
         this.weight = weight;
         this.height = height;
         this.nationality = nationality;
@@ -14,13 +13,27 @@ class Person{
 
     //-- Métodos
     calcIMC(){
-        return Math.floor((this.weight / Math.pow(this.height,2))*10000);
+        let imc = (this.weight / Math.pow(this.height,2))*10000;
+        return Number(imc.toFixed(2));
     }
 
     calcAge(){
         let date = new Date();
         return date.getFullYear() - this.yearOfBirth;
     }
+
+    printAll(){
+        for (const attribute in this) {
+            console.log(`${attribute}-${this[attribute]}`);
+        }
+    }
 }
 
 
+//-- Pruebas
+let persona = new Person('Olivia','Fernandez',75,165,'Española',1998);
+let imc = persona.calcIMC();
+console.log(`IMC = ${imc}`);
+let edad = persona.calcAge();
+console.log(`${persona.name} tiene ${edad} años.`);
+persona.printAll();
