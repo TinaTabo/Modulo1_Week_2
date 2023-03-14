d0d613c1c7e487acfacb3c62bb0db6872b4dd5a9
Cristina Taboada <cris.taboada98@gmail.com>
2023-03-14 13:36:05 +0100

    Reto 3

--- POO/poo.js
+++ POO/poo.js
@@ -1,18 +1,25 @@
 //-- Clases
 class Person{
-    // Constructor
-    constructor(name,lastName,age,weight,height,nationality){
-        this.nombre = name;
-        this.apellido = lastName;
-        this.edad = age;
-        this.peso = weight;
-        this.altura = height;
-        this.nacionalidad = nationality;
+    //-- Constructor
+    constructor(name,lastName,age,weight,height,nationality,yearOfBirth){
+        //-- Atributos
+        this.name = name;
+        this.lastName = lastName;
+        this.age = age;
+        this.weight = weight;
+        this.height = height;
+        this.nationality = nationality;
+        this.yearOfBirth = yearOfBirth;
     }
 
-    // Métodos
+    //-- Métodos
     calcIMC(){
-        return Math.floor((this.peso / Math.pow(this.altura,2))*10000);
+        return Math.floor((this.weight / Math.pow(this.height,2))*10000);
+    }
+
+    calcAge(){
+        let date = new Date();
+        return date.getFullYear() - this.yearOfBirth;
     }
 }
 
